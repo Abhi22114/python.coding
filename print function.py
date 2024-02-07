c0448beeb58b484793b85aa016e301f450566782
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-02-07 23:40:54 +0530

    Update print function.py

--- print function.py
+++ print function.py
@@ -1,1 +1,35 @@
 
+
+#printfunction
+
+
+# print function is used to display the output
+# or print something
+
+
+print("hello world")
+
+
+
+
+# End prameters 
+# End parameters is used to avoid new line 
+
+# Abhijeet kumar
+print("Abhijeet",end=" dewelliers ")
+print("Kumar")
+name="abhijeet"
+kumar="singh"
+# print(name,end=" ")
+# print(kumar,end=" ")
+ 
+# my_name_is="Raja"
+# my_sister_name_is="Shanu and shagun"
+# print(my_name_is,end=" ")
+# print(my_sister_name_is,end=" ")
+n1="abhijeet"
+n2="kumar"
+print(n1,end=" ")
+print(n2,end=" ")
+print("my name is ",n1, n2,end=" ")
+
