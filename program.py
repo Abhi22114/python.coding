9db8bab67ce553b784184d7da9a891ded187558f
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-02-07 21:16:37 +0530

    Update program.py

--- program.py
+++ program.py
@@ -1,3 +1,90 @@
+#loops
+#while loop and for loop
+#loop save our time
+
+#print(numbers)b/w 1 to 60
+
+#print(1)
+#x=1
+#while x<6:
+    #print(x)
+    #x=x+1
+#break statement 
+#x=1
+#while x < 10:
+
+    #print(x)
+    #if x==6:
+       # break
+   # x+=1
+
+ #continue
+
+#X=0
+#while X<10:
+   # X+=1
+   # if X==7:
+      #  continue #you skip tye current interation and  move on to the next
+  #  print(X)
+#else:
+    #print('out of rage')
+
+ #for loop
+#num=[10,20,30,40]  
+#for x in num:
+    #print(x)
+                      #for string valve# 
+#for x in 'hello':
+     #print(x)
+#num=[10,20,30,40,50]
+#for x in num :
+    #print(x)
+    #if (x==30):
+        #break
+
+#for x in range (2,10,2):
+    #print(x)
+#for x in range(5):
+    #if (x==3):break
+    #print(x)
+#else:
+    #print('finally the process is finished')
+    
+# nested loop(loop inside loop)
+
+#arr1=[1,2]
+#arr2=[5,6]
+#for x in arr1:
+    #for y in arr2:
+        #print(x,y)
+#inner loop gets excuted once for each interation of the outer loop
+
+#for x in [10,20,30]:
+    #pass
+#print(x)
+#print("heloo")
+#functions
+
+
+
+
+
+
+#a function either does something to it return ssomething (a bolck code that is excuted )
+#def my_func():
+    #print('hello world')
+
+#my_func()
+#my_func()
+
+#data into function as arguments 
+
+
+
+def my_func(fName,lName,age):
+    print('My name is'+ fName + lName + "My age is "+age,)
+my_func("jhon","cena",'40')
+
 
 # Write Python 3 code in this online editor and run it.
 #function:-function is a block of code that only runs when it called ,,manage the 
