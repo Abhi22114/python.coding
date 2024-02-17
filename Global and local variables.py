5a96968b80f0b6d0923146a9bfa0302eca80810b
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-02-17 20:01:45 +0530

    Add files via upload

---
+++ Global and local variables.py
@@ -0,0 +1,37 @@
+# Global Variables
+# Variables that are created outside of a function (as in all of the examples above) are known as global variables.
+
+# Global variables can be used by everyone, both inside of functions and outside.
+
+# Example
+# Create a variable outside of a function, and use it inside the function
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+# If you create a variable with the same name inside a function, this variable will be 
+# local, and can only be used inside the function. The global variable with the same name 
+# will remain as it was, global and with the original value.
+
+# Example
+# Create a variable inside a function, with the same name as the global variable
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+
+# local variables
+
+# if we create a varribles inside a function can be used only inside it
+# but we cannot use it outside a function and these type of  
---
+++ escape sequance.py
@@ -0,0 +1,6 @@
+# "My name " is abhijeet(print this as a output)
+print("\"My name\" is abhijeet")
+
+print("Abhijeet\\") #for printing backslsh as a input
+print("abhijeet \n kumar") #\n is used to print the given test in a new line
+print("Abhijeet\tkumar")# \t is used to give the space b/w to text
